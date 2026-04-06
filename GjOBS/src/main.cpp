24cee60f30691696a26541aa8d5042960a1ca7c9
MrGOLIJA <sarsembaevgoergij@gmail.com>
2026-04-06 17:44:02 +0600

    Добавлен qml файл для GUI, пока пустой

--- GjOBS/src/main.cpp
+++ GjOBS/src/main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QTimer>
+#include <QQmlApplicationEngine>
 
 #include "ffmpegManager.h"
 #include "outputdevice.h"
@@ -9,6 +10,8 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    /*QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/GjOBS/src/qml/main.qml")));*/
 
     QTimer* _timer = new QTimer(nullptr);
     _timer->setInterval(1000 / 60);
@@ -21,14 +24,10 @@ int main(int argc, char *argv[])
     ScreenRecorder* screen = new ScreenRecorder(nullptr, _timer);
     OutputDevice* audio = new OutputDevice(nullptr);
     FfmpegManager* ffmpeg = new FfmpegManager(audio, screen,settings);
-    int width = GetSystemMetrics(SM_CXSCREEN);
-    int height = GetSystemMetrics(SM_CYSCREEN);
-
-    ffmpeg->setSizeScreen({width,height});
     ffmpeg->initFFMPEG("test.mp4");
 
     QTimer* ntimer = new QTimer(nullptr);
-    ntimer->setInterval(10000);
+    ntimer->setInterval(5000);
     ntimer->setSingleShot(true);
 
     QObject::connect(ntimer, &QTimer::timeout, [ffmpeg]() {
---
+++ GjOBS/src/qml/main.qml
@@ -0,0 +1,19 @@
+﻿import QtQuick
+import QtQuick.Window
+
+Window {
+    visible: true
+    width: 200
+    height: 100
+    title: "GjOBS"
+
+    Rectangle {
+        anchors.fill: parent
+        color: "red"
+
+        Text {
+            anchors.centerIn: parent
+            text: "Hello, World!"
+        }
+    }
+}
